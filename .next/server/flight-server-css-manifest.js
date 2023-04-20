5b97ad745645ea3f4bbd7b3d2f82d5b84d2a641a
Tiago Silva Medeiros <tiagosilvam94@gmail.com>
2023-04-20 20:19:32 -0300

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/layout.tsx": [
+      "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/(figuras)/2d/reta/page": [
+      "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/globals.css"
+    ],
+    "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/(figuras)/pixel/page": [
+      "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/globals.css"
+    ],
+    "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/page": [
+      "/Users/tiago/Documents/GitHub/projeto-cs-ts-new/src/app/globals.css"
+    ]
+  }
+}
